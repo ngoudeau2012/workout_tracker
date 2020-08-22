50fa7d49ab4678336d647c7014a824d8d2320d7f
Nicholas Goudeau <ngoudeau2012@gmail.com>
2020-08-22 14:25:50 -0700

    adjustments for deployement

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-require("dotenv")
+require("dotenv").config()
 
 const PORT = process.env.PORT || 3000;
 const app = express();
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI || process.env.URI, {
   useNewUrlParser: true
 });
 
